25cf054fb84366d71e166282e6e8068589c473ed
Jesus <ivoice666@hotmail.com>
2025-09-01 21:32:47 +0200

    Add additional symbol types

--- srcs/print_symbols.c
+++ srcs/print_symbols.c
@@ -6,24 +6,29 @@
 /*   By: jesuserr <jesuserr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 12:14:54 by jesuserr          #+#    #+#             */
-/*   Updated: 2025/09/01 13:39:39 by jesuserr         ###   ########.fr       */
+/*   Updated: 2025/09/01 21:29:06 by jesuserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nm.h"
 
 // Determines the symbol type character for 32-bit ELF symbols following nm's
-// convention: U=undefined, C=common, A=absolute, T/t=text (executable code),
+// convention: U=undefined, C=common, A/a=absolute, T/t=text (executable code),
 // B/b=BSS (uninitialized data), D/d=data (initialized), R/r=read-only data,
-// W/w=weak symbols. Uppercase indicates global binding, lowercase is local.
+// W/w=weak symbols, V/v=weak object symbols, i=indirect function (GNU IFUNC).
+// Uppercase indicates global binding, lowercase is local.
 static char	get_symbol_type_32(Elf32_Sym *symbol, Elf32_Shdr *sections)
 {
 	Elf32_Shdr	*section;
 
+	if (ELF32_ST_TYPE(symbol->st_info) == STT_GNU_IFUNC)
+		return ('i');
 	if (ELF32_ST_BIND(symbol->st_info) == STB_WEAK)
 	{
 		if (symbol->st_shndx == SHN_UNDEF)
 			return ('w');
+		if (ELF32_ST_TYPE(symbol->st_info) == STT_OBJECT)
+			return (ELF32_ST_BIND(symbol->st_info) == STB_LOCAL) ? 'v' : 'V';
 		return ('W');
 	}
 	if (symbol->st_shndx == SHN_UNDEF)
@@ -31,7 +36,7 @@ static char	get_symbol_type_32(Elf32_Sym *symbol, Elf32_Shdr *sections)
 	if (symbol->st_shndx == SHN_COMMON)
 		return ('C');
 	if (symbol->st_shndx == SHN_ABS)
-		return ('A');
+		return (ELF32_ST_BIND(symbol->st_info) == STB_LOCAL) ? 'a' : 'A';
 	if (symbol->st_shndx >= SHN_LORESERVE)
 		return ('?');
 	section = &sections[symbol->st_shndx];
@@ -53,36 +58,43 @@ void	print_symbols_32(t_data *data)
 {
 	Elf32_Sym	*sym;
 	t_list		*list;
+	char		type;
 
 	list = data->sym_list;
 	while (list)
 	{
 		sym = (Elf32_Sym *)list->content;
-		if (sym->st_value == 0)
+		type = get_symbol_type_32(sym, data->elf32_sec_table);
+		if (sym->st_value == 0 && (type == 'U' || type == 'w'))
 			print_char_n_times(' ', HEX_DIGITS_32 + 1);
 		else
 		{
 			print_char_n_times('0', HEX_DIGITS_32 - count_hex_digits(sym->st_value));
 			ft_printf("%x ", sym->st_value);
 		}
-		ft_printf("%c ", get_symbol_type_32(sym, data->elf32_sec_table));
+		ft_printf("%c ", type);
 		ft_printf("%s\n", &data->str_table[sym->st_name]);
 		list = list->next;
 	}
 }
 
 // Determines the symbol type character for 64-bit ELF symbols following nm's
-// convention: U=undefined, C=common, A=absolute, T/t=text (executable code),
+// convention: U=undefined, C=common, A/a=absolute, T/t=text (executable code),
 // B/b=BSS (uninitialized data), D/d=data (initialized), R/r=read-only data,
-// W/w=weak symbols. Uppercase indicates global binding, lowercase is local.
+// W/w=weak symbols, V/v=weak object symbols, i=indirect function (GNU IFUNC).
+// Uppercase indicates global binding, lowercase is local.
 static char	get_symbol_type_64(Elf64_Sym *symbol, Elf64_Shdr *sections)
 {
 	Elf64_Shdr	*section;
 
+	if (ELF64_ST_TYPE(symbol->st_info) == STT_GNU_IFUNC)
+		return ('i');
 	if (ELF64_ST_BIND(symbol->st_info) == STB_WEAK)
 	{
 		if (symbol->st_shndx == SHN_UNDEF)
 			return ('w');
+		if (ELF64_ST_TYPE(symbol->st_info) == STT_OBJECT)
+			return (ELF64_ST_BIND(symbol->st_info) == STB_LOCAL) ? 'v' : 'V';
 		return ('W');
 	}
 	if (symbol->st_shndx == SHN_UNDEF)
@@ -90,7 +102,7 @@ static char	get_symbol_type_64(Elf64_Sym *symbol, Elf64_Shdr *sections)
 	if (symbol->st_shndx == SHN_COMMON)
 		return ('C');
 	if (symbol->st_shndx == SHN_ABS)
-		return ('A');
+		return (ELF64_ST_BIND(symbol->st_info) == STB_LOCAL) ? 'a' : 'A';
 	if (symbol->st_shndx >= SHN_LORESERVE)
 		return ('?');
 	section = &sections[symbol->st_shndx];
@@ -112,19 +124,21 @@ void	print_symbols_64(t_data *data)
 {
 	Elf64_Sym	*sym;
 	t_list		*list;
+	char		type;
 
 	list = data->sym_list;
 	while (list)
 	{
 		sym = (Elf64_Sym *)list->content;
-		if (sym->st_value == 0)
+		type = get_symbol_type_64(sym, data->elf64_sec_table);
+		if (sym->st_value == 0 && (type == 'U' || type == 'w'))
 			print_char_n_times(' ', HEX_DIGITS_64 + 1);
 		else
 		{
 			print_char_n_times('0', HEX_DIGITS_64 - count_hex_digits(sym->st_value));
 			ft_printf("%x ", sym->st_value);
 		}
-		ft_printf("%c ", get_symbol_type_64(sym, data->elf64_sec_table));
+		ft_printf("%c ", type);
 		ft_printf("%s\n", &data->str_table[sym->st_name]);
 		list = list->next;
 	}
